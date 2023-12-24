865a9cde78c0ef4373e5061614fa80cdafd1596b
RKet <kirito.7.3.9.5@gmail.com>
2023-12-24 22:36:41 +0545

    popup

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 app=Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/' , methods=['GET' , 'POST'])
 
 def calculator():
     if request.method == 'POST':
@@ -11,6 +11,6 @@ def calculator():
         area= length * breadth
     else:
         length=breadth=area=None
-    return render_template("index.html", length=length, breadth=breadth, area=area)
+    return render_template('index.html', length=length, breadth=breadth, area=area)
 if __name__=='__main__':
     app.run(debug=True)
